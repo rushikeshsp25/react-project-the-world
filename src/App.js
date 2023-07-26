bf0eb2aa238d9a0541f77199f466e9c6ac49f447
rushikesh-fluxon <rushikesh@fluxon.com>
2023-07-26 09:33:11 +0530

    working country app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Pages/Home';
+import CountryDetail from './Pages/CountryDetail';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/countries/:countryCode' element={<CountryDetail/>} />
+          <Route path='*' element={<h2>404 Page not found</h2>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
